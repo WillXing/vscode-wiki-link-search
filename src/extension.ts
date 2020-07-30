16ecf900c34d22633cf2e9d538a896bad9c921f9
Will Huang <will.xing@outlook.com>
2020-07-30 23:29:10 +1200

    wiki link main function

--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,49 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
   let disposable = vscode.commands.registerCommand('wiki-link.search', async () => {
-    // The code you place here will be executed every time your command is executed
-    // Display a message box to the user
     
-    const inputBoxOptions: vscode.InputBoxOptions = {
-      placeHolder: "search wiki"
-    };
-
-    const search = await vscode.window.showInputBox(inputBoxOptions);
-    if (!search) {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+      vscode.window.showInformationMessage("Can't insert. No active editor open.");
       return;
     }
-    const response = await axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${encodeURI(search)}&limit=4&format=json`);
-    vscode.window.showQuickPick(response.data[1]);
-    vscode.window.showInformationMessage(search);
+
+    const document = editor.document;
+    const selection = editor.selection;
+
+    const word = document.getText(selection);
+    
+    let response: AxiosResponse;
+    try {
+      response = await axios.get(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${encodeURI(word)}&limit=4&format=json`);
+    } catch (e) {
+      vscode.window.showInformationMessage(`Search fail`);
+      throw e;
+    }
+
+    const links = response.data[1].map((title: string, index: string) => ({
+      label: title,
+      description: response.data[3][index]
+    }));
+
+    const pickOption: vscode.QuickPickOptions = {
+      canPickMany: false,
+      onDidSelectItem: (item: vscode.QuickPickItem) => {
+      }
+    };
+
+    const selectedItem =
+      await vscode.window.showQuickPick<vscode.QuickPickItem>(links, pickOption);
+    if (!selectedItem) { return; }
+    editor.edit(editBuilder => {
+      editBuilder.replace(selection, `[${selectedItem.label}](${selectedItem.description})`);
+    });
   });
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() { }
